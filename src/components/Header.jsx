b3ee4405d6f02ba3664e8bcbcf5b70cace4d954b
Vipulpandey007 <vipulpandey708@gmail.com>
2024-06-26 16:14:42 +0530

    upto url deletion completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,8 +51,10 @@ const Header = () => {
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
-                  <LinkIcon className="mr-2 h-4 w-4" />
-                  My Links
+                  <Link to="/dashboard" className="flex">
+                    <LinkIcon className="mr-2 h-4 w-4" />
+                    My Links
+                  </Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem className="text-red-500">
                   <LogOut className="mr-2 h-4 w-4" />
